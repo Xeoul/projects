c71d3f5d907c326208e63246e187f36620b10da7
Xeoul <32425797+Xeoul@users.noreply.github.com>
2021-01-20 20:03:35 -0800

    Add files via upload

---
+++ CISP 400 Projects/pig.cpp
@@ -0,0 +1,18 @@
+// pig.cpp
+// Vincent Lam, CISP 400
+// 1/20/21
+void programGreeting();
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "new line of code\n";
+    programGreeting();
+    return 0;
+}
+
+void programGreeting()
+{
+    cout << "Welcome to the pig program\n";
+    cout << "Rules: \n";
+}
